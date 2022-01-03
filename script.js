8c965519f0b4187c8c8b1ce36cde039d4e6b662a
wheatwel <xantares301@gmail.com>
2022-01-02 22:59:43 -0500

    A few adjusts

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ setTimeout(() => {
 
             if(i < 5){
 
-                playerSelection = prompt("Please, choose: -Rock- -Paper- or -Scissors-").toLowerCase();
+                playerSelection = prompt("Choose: Rock,  Paper   or   Scissors").toLowerCase();
 
                 function computerChoose(){
                     let a, b, c, computerAnswer;
@@ -78,8 +78,8 @@ setTimeout(() => {
                 }
                 console.log("%c -----------------------------------", "color:tomato;");
                 console.log(playGame(playerSelection, computerPlay));
-                console.log(`%c ♥ Your score is: ${playerScore}`, `color:pink;font-size:1rem;background: linear-gradient(#f44161, #4198e5);font-weight:bold;`);  
-                console.log(`%c ♦ Computer score is: ${computerScore}`, `color:pink;font-size:1rem;background-color:blue;background: linear-gradient(217deg, rgba(255,0,0,.8), rgba(255,0,0,0) 70.71%),
+                console.log(`%c ♥ Your score is: ${playerScore} `, `color:pink;font-size:1rem;background: linear-gradient(#f44161, #4198e5);font-weight:bold;`);  
+                console.log(`%c ♦ Computer score is: ${computerScore} `, `color:pink;font-size:1rem;background-color:blue;background: linear-gradient(217deg, rgba(255,0,0,.8), rgba(255,0,0,0) 70.71%),
                 linear-gradient(127deg, rgba(0,255,0,.8), rgba(0,255,0,0) 70.71%),
                 linear-gradient(336deg, rgba(0,0,255,.8), rgba(0,0,255,0) 70.71%);font-weight:bold;`);
 
